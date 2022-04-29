3a465fc9a2cb107c9464b7aa765304b51f057caa
DaniRicher <richerdani3@gmail.com>
2022-04-29 16:19:13 -0500

    Marcadores

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl'
+
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
-  styles: [
-  ]
+  styles: [`
+      #mapa{
+        width:100%;
+        height:100%;
+      }
+  `]
 })
 export class FullScreenComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+    const map = new mapboxgl.Map({
+    container: 'mapa',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-74.17373336589517,11.226594511731179],
+    zoom: 17
+    });
   }
 
 }
--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
-  ]
+  styles: [`
+  .mapa-container{
+    width:100%;
+    height:100%;
+  }
+  .row{
+    background-color:white;
+    border-radius:5px;
+    bottom:50px;
+    left:50px;
+    padding:10px;
+    position:fixed;
+    z-index:999;
+    width:280px;
+  }
+`]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit , OnDestroy{
+
+  @ViewChild('mapa')divMapa!: ElementRef;
+  mapa!:mapboxgl.Map;
+  zoomLevel:number=14;
+  center:[number,number]=[-74.17373336589517,11.226594511731179]
 
   constructor() { }
+  ngOnDestroy(): void {
+    this.mapa.off('zoom',()=>{})
+    this.mapa.off('zoomend',()=>{})
+    this.mapa.off('move',()=>{})
+  }
 
-  ngOnInit(): void {
+  
+  ngAfterViewInit(): void {
+
+
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+      });
+
+      this.mapa.on('zoom', (ev)=>{
+        this.zoomLevel=this.mapa.getZoom();
+      });
+
+      this.mapa.on('zoomend', (ev)=>{
+        if(this.mapa.getZoom()>18){
+          this.mapa.zoomTo(18);
+        }
+      });
+      //Moviento del mapa
+      this.mapa.on('move',(event)=>{
+        const target=event.target;
+        const {lng, lat}=target.getCenter();
+        this.center=[lng,lat]
+      });
+  }
+
+  zoomIn(){
+    this.mapa.zoomIn();
+  }
+  zoomOut(){
+    this.mapa.zoomOut();
+  }
+  zoomCambio(valor:string){
+    this.mapa.zoomTo(Number(valor))
   }
 
 }
